b08d546f0777d1c5063027069605442af580544f
Sangeethux <sangeethnipun994@gmail.com>
2022-10-15 00:24:17 +0530

    Mid Quiz Tas 1 and 2

---
+++ Java/jvscrptquiz.js
@@ -0,0 +1,17 @@
+var university ="AMDT"
+var myName ="Sangeeth"
+console.log("Hi I am ",myName,"from",university);
+
+
+
+
+var movies=["harry potter","narnia","fast and furious","Interstellar","aliens"];
+var forthArray = movies[3];
+if (forthArray.charAt(0) ==="I") {
+
+    console.log("Yay");
+}
+
+else{
+    console.log("Nay");
+}
